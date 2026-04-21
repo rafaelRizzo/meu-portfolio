9949f744e0530dfda5681eff2f67e287ab5205c7
Rafael Rizzo <92104771+rafaelRizzo@users.noreply.github.com>
2026-04-21 16:16:06 -0300

    fix: atualizando pacotes do projeto e ajuste no about
    - Agora o calculo de experiencia e anos de mercado é feito automaticamente, dessa maneira no próximo ano, irá comparar com o ano atual e quem irá acessar a pagina verá atualizado.

--- src/components/TabSection/About/index.tsx
+++ src/components/TabSection/About/index.tsx
@@ -1,17 +1,20 @@
+"use client"
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
 
 export const About = () => {
+    const currentYear = new Date().getFullYear()
+    const totalExp = currentYear - 2017
+    const marketExp = currentYear - 2021
+
     return (
         <Card>
             <CardHeader>
                 <CardTitle>Sobre</CardTitle>
-                <CardDescription>
-                    Um pouco sobre mim
-                </CardDescription>
+                <CardDescription>Um pouco sobre mim</CardDescription>
             </CardHeader>
             <CardContent className="grid gap-6 text-xs">
-                <p>8 anos de experiência e 4 anos de mercado, atuando desde os 17 anos.</p>
-                <p>Expecialista em desenvolvimento de sistemas web, landing pages, API RESTful, automações e muito mais.</p>
+                <p>{totalExp} anos de experiência e {marketExp} anos de mercado, atuando desde os 17 anos.</p>
+                <p>Especialista em desenvolvimento de sistemas web, landing pages, API RESTful, automações e muito mais.</p>
                 <p>Minha missão: fazer o difícil parecer simples.</p>
             </CardContent>
         </Card>
